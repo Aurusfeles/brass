10d74a64ebcf9d79bf02dc27665a81e4bb438a99
Aurelien <aurelien.dupont@inrae.fr>
2022-11-22 16:51:42 +0100

    WIP:adding cards mechanics

--- components/my_hand.vue
+++ components/my_hand.vue
@@ -1,25 +1,48 @@
 <template>
-  <div class="hand">
-    <div
-      class="card"
-      v-for="(card, index) in content"
-      :key="index"
-      @click="card_click()"
-    >
-      {{ card.type == "location" ? card.location : card.building }}
+  <div>
+    <div class="hand">
+      <div
+        class="card"
+        v-for="(card, index) in content"
+        :key="index"
+        @click="card_click(card)"
+      >
+        {{ card.type == "location" ? card.location : card.building }}
+      </div>
+    </div>
+    <div class="possibilities">
+      <div v-for="(v, k) in possibilities" :key="k">
+        {{ v }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      possibilities: [],
+    };
+  },
   props: {
     content: {
       type: Array,
     },
   },
   method: {
-    card_click() {},
+    card_click(card) {
+      this.possibilities.push(this.build_possibilities(card));
+    },
+    build_possibilities(card) {
+      if (card.type == "location") {
+        return build_possibilities_for_location(card.location);
+      } else {
+        return build_possibilities_for_building(card.building);
+      }
+    },
+    build_possibilities_for_location(location) {},
+    build_possibilities_for_building(building) {},
   },
 };
 </script>
